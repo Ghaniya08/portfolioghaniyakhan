d100a97ac95f5aabcb3b40a42ae536b4a11ebe70
Ghaniya khan <165418041+Ghaniya08@users.noreply.github.com>
2024-11-09 20:26:52 +0500

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,4 +1,3 @@
-import { div } from 'framer-motion/client';
 import React from 'react'
 import {FaYoutube , FaCode , FaDesktop , FaBook} from "react-icons/fa";
 const SerList = [
@@ -30,12 +29,10 @@ const Services = () => {
      <div className='w-[80%] mx-auto grid grid-cols-1  md:grid-cols-2 lg:grid-cols-2 gap-12 items-center'>
      {SerList.map((service , index )=>(
        <div key={index} className='bg-gradient-to-r from-pink-500 via-purple-600 to-purple-600 animate-gradient-xy p-6 rounded-lg hover:scale-105 transition-all duration-300'>
-          
-            <h1 className='text-white text-3xl md:ml-2 md:mb-2'>{service.icon}</h1>
+          <h1 className='text-white text-3xl md:ml-2 md:mb-2'>{service.icon}</h1>
             <h1 className="text-white text-2xl mb-2 font-semibold">{service.skill}</h1>
             <p className='text-white'>{service.description}</p>
-           
-        </div>
+           </div>
       ))}
      </div>
     </div>
