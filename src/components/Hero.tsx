f87fb116a17db602309dd2b7add89fd5c4056965
Ghaniya khan <165418041+Ghaniya08@users.noreply.github.com>
2024-11-09 20:39:15 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ import AnimationComp from './AnimationComp'
 
 const Hero = () => {
   return (
-    <div className='mb-[135px] mt-[140px]' id='Hero'>
+    <div className='mb-[135px] lg:mt-[140px]' id='Hero'>
      <AnimationComp size={800} rotation={-72}>
         <PiButterflyLight className='size-16 text-pink-700/80 animate-bounce lg:block hidden'/>
       </AnimationComp>
@@ -36,8 +36,11 @@ const Hero = () => {
       <AnimationComp size={520} rotation={0}>
         <div className='size-[10px] bg-pink-700/30 rounded-full lg:block hidden '/>
       </AnimationComp>
-     <div className='lg:flex h-screen lg:px-[250px] lg:gap-[50px] px-6 '>
-     <div className='mt-[50px]'>
+     <div className='lg:flex lg:px-[250px] lg:gap-[50px] px-6 '>
+     <div >
+        <Image src={home} alt='homepage' width={800} height={800} className=' w-[400px] h-[300px] block lg:hidden'/>
+      </div>
+     <div className='lg:mt-[50px] -mt-2'>
         <h1 className='scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl bg-gradient-to-t bg-clip-text from-purple-500 to-pink-700 text-transparent'>Ghaniya Khan</h1>
         <h1 className="scroll-m-20 pb-2 text-xl font-semibold tracking-tight first:mt-0 bg-gradient-to-t bg-clip-text from-purple-500 to-pink-700 text-transparent">
             <Typewriter
@@ -49,13 +52,13 @@ const Hero = () => {
                 }
             }/>
         </h1>
-        <p className='lg:w-[400px] lg:text-[15px] text-[13px] font-semibold bg-gradient-to-t bg-clip-text from-purple-500 to-pink-700 text-transparent'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos in dicta enim sequi voluptate nesciunt provident non dolores consectetur magnam nihil, animi necessitatibus maiores deleniti tempore sed vero eos possimus.</p>
+        <p className='lg:w-[400px] lg:text-[15px] text-[10px] font-semibold bg-gradient-to-t bg-clip-text from-purple-500 to-pink-700 text-transparent'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos in dicta enim sequi voluptate nesciunt provident non dolores consectetur magnam nihil, animi necessitatibus maiores deleniti tempore sed vero eos possimus.</p>
       </div>
       <div >
-        <Image src={home} alt='homepage' width={800} height={800} className=' w-[400px] h-[300px] '/>
+        <Image src={home} alt='homepage' width={800} height={800} className=' w-[400px] h-[300px] lg:block hidden'/>
       </div>
      </div>
-     <div className='lg:-mt-[380px] -mt-[50px] text-center'>
+     <div className='lg:-mt-[380px] mt-2  text-center'>
       <button className='rounded-xl bg-gradient-to-t from-purple-700 to-pink-600 lg:w-[150px] lg:h-[50px] w-[100px] h-[40px] lg:text-[15px] text-[10px] mr-4'>Explore My work</button>
       <button className='rounded-xl bg-gradient-to-t from-purple-700 to-pink-600 lg:w-[150px] lg:h-[50px] w-[100px] h-[40px] lg:text-[15px] text-[10px] ml-4'>Lets Contact</button>
      </div> 
